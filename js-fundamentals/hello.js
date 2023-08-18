402aa9955059aa3c371a401d56df0f6cc6351449
sherrychenxy <csherry1224@gmail.com>
2023-08-18 14:33:32 +0000

    funcation example

---
+++ js-fundamentals/hello.js
@@ -0,0 +1,11 @@
+const sayHello = function(name){
+  console.log("Hello, " + name);
+}
+sayHello("xing");
+sayHello("wu");
+/*
+const countNum = function(num){
+  return num * num;
+}
+console.log(countNum(5));
+*/
